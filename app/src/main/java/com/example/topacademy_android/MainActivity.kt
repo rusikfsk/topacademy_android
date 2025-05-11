e5865dfb77cb4251d1f8f0c4e78da13228232482
Ashipilov <Thesol590@yandex.ru>
2025-05-12 00:14:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/topacademy_android/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.topacademy_android
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.topacademy_android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val ON_CREATE = "ON_CREATE"
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Log.i(ON_CREATE, "Активити создана!")
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
